4d91167ee9aa67e645c6cb0fcb09cbefae0f321e
nadajinny <jinsun23007@gmail.com>
2025-12-27 01:12:07 +0900

    feat: 입력과 출력 객체 생성 및 Controller에 전달

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import racingcar.controller.RacingCarController
+import racingcar.view.ConsoleInputView
+import racingcar.view.ConsoleOutputView
+
 fun main() {
     // TODO: 프로그램 구현
+    val inputView = ConsoleInputView()
+    val outputView = ConsoleOutputView()
+
+    val racingCar = RacingCarController(inputView, outputView)
+    racingCar.run()
 }
