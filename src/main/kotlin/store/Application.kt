87d6b12f19ad51cd12b9954b33376ebe0d945c3f
nadajinny <jinsun23007@gmail.com>
2026-01-03 22:17:38 +0900

    feat: storecontroller에게 전달

--- src/main/kotlin/store/Application.kt
+++ src/main/kotlin/store/Application.kt
@@ -1,5 +1,7 @@
 package store
 
+import store.controller.StoreController
+
 fun main() {
-    // TODO: 프로그램 구현
+    StoreController().run()
 }
---
+++ src/main/kotlin/store/controller/StoreController.kt
@@ -0,0 +1,13 @@
+package store.controller
+
+import store.view.InputView
+import store.view.OutputView
+
+class StoreController {
+    private val input = InputView()
+    private val output = OutputView()
+
+    fun run() {
+
+    }
+}
