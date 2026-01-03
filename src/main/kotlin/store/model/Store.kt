a183d165ecf9913d222f1ab07a84e6a426177de2
nadajinny <jinsun23007@gmail.com>
2026-01-03 22:17:53 +0900

    feat: 기본적인 형태 구현

---
+++ src/main/kotlin/store/model/Store.kt
@@ -0,0 +1,9 @@
+package store.model
+
+class Store {
+    private val carts = mutableListOf<Cart>()
+    private val products = mutableListOf<Product>()
+    private val promotions = mutableListOf<Promotion>()
+
+
+}
